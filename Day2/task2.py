26c1cc41d012c62f837a91418630779838ba2ee7
Tahsinjubayer <tahsinjubayer333@gmail.com>
2025-05-22 11:53:11 +0600

    Added Day 2 tasks

---
+++ Day2/task2.py
@@ -0,0 +1,9 @@
+print("Welcome to the band name generator.")
+
+city = input("Which city did you grow up in?\n")
+
+pet = input("Whats your pets name?\n")
+
+Band_name = city + " " + pet
+
+print("Your band name could be " + Band_name)
